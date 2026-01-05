07385b4a030e22a0179f6bc326e4a5099df3e5a3
komalkirti <40974849+komalkirti@users.noreply.github.com>
2026-01-05 22:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (32.32%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int k=n-1;
+        int cnt=0;
+        while(k>=0 && s[k]==' ')k--;
+        cout<<k<<endl;
+        while(k>=0 && s[k]!=' '){
+            k--;
+            cnt++;
+        }
+        return cnt;
+    }
+};
