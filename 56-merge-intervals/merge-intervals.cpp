03488320180928c95efd399bbf956df445ab7722
komalkirti <40974849+komalkirti@users.noreply.github.com>
2026-01-10 22:01:24 +0530

    Time: 7 ms (41.74%) | Memory: 23.7 MB (93.61%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>>merged;
+        int n=intervals.size();
+
+        for(int i=0;i<n;i++){
+
+            if(merged.empty() || (merged.back()[1]<intervals[i][0])){
+                merged.push_back(intervals[i]);
+            }
+            else{
+                int endInterval=max(merged.back()[1],intervals[i][1]);
+                merged.back()[1]=endInterval;
+            }
+        }
+        return merged;
+        
+    }
+};
